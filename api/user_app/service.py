9ed4e93c11f3c29a88585b76338a78ba77debfe0
Renildo15 <renildorabi@ufrn.edu.br>
2024-10-15 19:27:58 -0300

    imp:verificando se o aluno está na lista #17

--- api/user_app/service.py
+++ api/user_app/service.py
@@ -3,10 +3,17 @@ from api.user_app.utils.students_list import get_students_list
 
 
 class UserService:
-    def get_user_in_students_list(self, matric_number):
-        students_list = get_students_list()
 
-        for student in students_list:
+    def __init__(self):
+        self.students_list = get_students_list()
+
+    def get_user_in_students_list(self, matric_number):
+        for student in self.students_list:
             if student["matriculation_student"] == matric_number:
                 return student
         return None
+    def is_student_in_list(self, matric_number):
+        for student in self.students_list:
+            if student["matriculation_student"] == matric_number:
+                return True
+        return False
