acfc692a4d40a81dacca4c2be00aab4fda01d570
Renildo15 <renildorabi@ufrn.edu.br>
2024-11-09 09:29:40 -0300

    feat: adicionando filtro ao notices #42

--- api/buslist_app/filters.py
+++ api/buslist_app/filters.py
@@ -10,3 +10,26 @@ def apply_filters_bus_list(queryset, request):
     queryset = queryset.filter(**filter_params)
 
     return queryset
+
+def apply_filters_notice(queryset, request):
+
+    viewed = request.query_params.get("viewed")
+    is_viewed = is_viewed_check(viewed)
+    
+
+    filter_params = {
+        "buslist": request.query_params.get("buslist"),
+        "viewed": is_viewed,
+    }
+
+    filter_params = {k: v for k, v in filter_params.items() if v is not None}
+    queryset = queryset.filter(**filter_params)
+
+    return queryset
+
+def is_viewed_check(viewed):
+    if viewed is not None and viewed == "true":
+        return True
+    elif viewed is not None and viewed == "false":
+        return False
+    return None
